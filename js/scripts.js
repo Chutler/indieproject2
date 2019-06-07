7d2ca5cad0614c18943f65f6cd31489ef360e672
Craig Hutler <craig@craighutler.com>
2019-06-06 20:52:16 -0700

    complete the necessary requirements for the project

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,54 @@
-$(document).ready(function(){
+// Function for getting an average score from the select inputs
+var score = function(income, math, operatingSys,beverage) {
+    return (income + math + operatingSys + beverage) / 4;
+  };
 
-    $("#form").submit(function() {
+$(document).ready(function(){
+    
+    $("#form").submit(function(event) {
 
         var firstName= $("input#firstName").val();
         var lastName = $("input#lastName").val();
         var email = $("input#email").val();
-        // var addressInput = $("input#address").val();
-        // var cityInput = $("input#city").val();
-        // // var stateInput = $("input#state").val();
-        // var zipCodeInput = $("input#zipCode").val();
-        // // var cityInput = $("input#city").val();
-        var program = "coool"
+        var income = parseInt($("select#income").val());
+        var math = parseInt($("select#math").val());
+        var operatingSys = parseInt($("select#operatingSys").val());
+        var beverage = parseInt($("select#beverage").val());
+        var term = $("input:radio[name=term]:checked").val();
 
-        console.log("First Name: " + firstName);
-        console.log("Last Name: " + lastName);
-        console.log("Email: " + email);
-        console.log("Email: " + email);
+        // call the score function with our form inputs
+        var result = score(income, math, operatingSys,beverage);
+
+        // leverage the resulting score to determine the program
+        var program;
+        if (result <= 2) {
+            (program = "Jr. Dev Ops");
+        } else if (result > 2 && result <= 3.25) {
+            (program = "Front-end Development");
+        } else {
+            (program = "Machine Learning / AI");
+        }
+
+        // console.log("First Name: " + firstName);
+        // console.log("Last Name: " + lastName);
+        // console.log("Email: " + email);
+        // console.log("Income: " + income);
+        // console.log("Math Level: " + math);
+        // console.log("OS: " + operatingSys);
+        // console.log("Beverage: " + beverage);
+        // console.log("Term: " + term);
+        // console.log("Score: " + score);
+        // console.log("Result: " + result);
     
         $(".firstName").text(firstName);
         $(".lastName").text(lastName);
         $(".email").text(email);
-        // $(".address").text(addressInput);
-        // $(".city").text(cityInput);
-        // // $(".state").text(stateInput);
-        // $(".zipCode").text(zipCodeInput);
-        // // $(".noun").text(cityInput);
-
-        // $('#SelectBoxId option:selected').text();
+        $(".term").text(term);
+        $(".program").text(program)
     
         $("#output").show();
-    
         event.preventDefault();
+        
     });
-
-    // Below is the code for the select element for State
-    $("#state").on("change",function(){
-        //Getting Value
-        var stateInput = $("#state :selected").val();
-        //Setting Value
-        $(".state").text(stateInput);
-    }); 
 
 });
